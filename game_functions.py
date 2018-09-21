8278f526960d88e6094ea1ab70eaac3bf5cdc280
andrew96nguyen <31872897+andrew96nguyen@users.noreply.github.com>
2018-09-21 15:17:15 -0700

    Updated 3 aliens

--- game_functions.py
+++ game_functions.py
@@ -216,21 +216,6 @@ def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     # Look for aliens hitting the bottom of the screen.
     check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-'''
-def get_number_aliens_x(ai_settings, alien_width):
-    """Determine the number of aliens that fit in a row."""
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
-    return number_aliens_x
-
-
-def get_number_rows(ai_settings, ship_height, alien_height):
-    """Determine the number of rows of aliens that fit on the screen."""
-    available_space_y = (ai_settings.screen_height - (4 * alien_height) - ship_height)
-    number_rows = int(available_space_y / (3.5 * alien_height))
-    return number_rows
-'''
-
 
 def create_alien1(ai_settings, screen, aliens, alien_number, row_number):
     """Create an alien, and place it in the row."""
@@ -265,22 +250,12 @@ def create_alien3(ai_settings, screen, aliens, alien_number, row_number):
 def create_fleet(ai_settings, screen, ship, aliens):
     """Create a full fleet of aliens."""
     # Create an alien, and find number of aliens in a row.
-    alien = Alien1(ai_settings, screen)
-    # number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
-    # number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+    # alien = Alien1(ai_settings, screen)
     number_aliens_x = 11
-    number_rows = 5
-
-    """
-    # Create the fleet of aliens.
-    for row_number in range(number_rows):
-        for alien_number in range(number_aliens_x):
-            create_alien(ai_settings, screen, aliens, alien_number, row_number)
-    """
 
     for alien_number in range(number_aliens_x):
-        create_alien1(ai_settings, screen, aliens, alien_number, 5)
-        create_alien1(ai_settings, screen, aliens, alien_number, 4)
-        create_alien2(ai_settings, screen, aliens, alien_number, 3)
-        create_alien2(ai_settings, screen, aliens, alien_number, 2)
-        create_alien3(ai_settings, screen, aliens, alien_number, 1)
+        create_alien1(ai_settings, screen, aliens, alien_number, 7)
+        create_alien1(ai_settings, screen, aliens, alien_number, 6)
+        create_alien2(ai_settings, screen, aliens, alien_number, 5)
+        create_alien2(ai_settings, screen, aliens, alien_number, 4)
+        create_alien3(ai_settings, screen, aliens, alien_number, 3)
